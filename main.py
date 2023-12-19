25b6de02334c36dd987f34ff8ce2c44a082713d8
Elanaly <drachandayo@gmail.com>
2023-12-19 16:19:08 +0900

    add command error function

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import discord
 from os import environ
+from discord.ext.commands import errors
+from discord.ext.commands.context import Context
 from dotenv import load_dotenv
 from discord.ext import commands
 
 extensions = [
     'cogs.ping',
-    'cogs.greetings'
+    'cogs.greetings',
+    'cogs.searchpic'
 ]
 
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix="*",
+            command_prefix=environ['PREFIX'],
             intents=discord.Intents.all()
         )
 
@@ -23,6 +26,9 @@ class MyBot(commands.Bot):
         print("起動しました")
         await bot.change_presence(activity=discord.Game(name="*で反応するよ", type=1))
         return
+    
+    async def on_command_error(self, context: Context, exception: Exception) -> None:
+        return await super().on_command_error(context, exception)
 
 
 
